536fa03389cfeb55e2e0e5b0fb6349a3edb68c82
Andre Fischer <andre.fischer@dsm.com>
2024-04-07 20:00:41 +0200

    added basic logic to connet a DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)  # Apply configuration from config.py
+db = SQLAlchemy(app) # line for DB
+migrate = Migrate(app, db) # line for db migration
 
 from app import routes
--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 
 class Config:
+    # Secret key against Csrf
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # Database
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
