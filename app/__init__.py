a404052ad2bdd8482b0982a5967596eba133b54c
Andre Fischer <andre.fischer@dsm.com>
2024-04-08 11:43:14 +0200

    added db migration directory

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+"""
+appliation setup
+"""
+
 app = Flask(__name__)
 app.config.from_object(Config)  # Apply configuration from config.py
 db = SQLAlchemy(app) # line for DB
