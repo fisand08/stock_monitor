30ebc9ce6beddfc078376fd8d5c99390793570c0
Andre Fischer <andre.fischer@dsm.com>
2024-04-09 08:24:29 +0200

    added profile-editor functionality, tested

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,21 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 import sqlalchemy as sa
 from flask_login import current_user, login_user, logout_user, login_required
 from flask import request
 from urllib.parse import urlsplit
+from datetime import datetime, timezone
+
+"""
+Logic executed before every request; executed before any view function
+"""
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc) # adding to db - no .add() needed here
+        db.session.commit() # committing to db
 
 @app.route('/')  # decorator for following function
 @app.route('/index')  # 2nd decorator
@@ -69,3 +79,20 @@ def user(username):
     alt_names = ['profil_' + x for x in ['guati','schatzi','schatzus','schatzo']]
 
     return render_template('user.html', user=user, alt_names=alt_names)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
